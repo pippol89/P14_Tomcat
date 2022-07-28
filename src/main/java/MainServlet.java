09545d3a44b6013172c1340f1f7d78c168faef0c
pippol89 <seedorf10@rambler.ru>
2022-07-28 09:27:03 +0300

    some changes

--- src/main/java/MainServlet.java
+++ src/main/java/MainServlet.java
@@ -2,6 +2,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class MainServlet extends HttpServlet {
@@ -9,5 +10,13 @@ public class MainServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //super.doGet(req, resp);
         System.out.println("we are in main method");
+
+        req.setAttribute("user", "Alexey");
+        req.getRequestDispatcher("main.jsp").forward(req, resp);
+
+        System.out.println("currentThreadId: " + Thread.currentThread().getId() + ", name: " + Thread.currentThread().getName());
+
+        HttpSession session = req.getSession();
+        System.out.println("Id session:" + session.getId());
     }
 }
