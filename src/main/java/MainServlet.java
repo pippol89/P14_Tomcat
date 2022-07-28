f518ff0cdba9d6efd774e14b7619384e0d718bf9
pippol89 <seedorf10@rambler.ru>
2022-07-28 07:30:05 +0300

    Tomcat lesson

---
+++ src/main/java/MainServlet.java
@@ -0,0 +1,13 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //super.doGet(req, resp);
+        System.out.println("we are in main method");
+    }
+}
